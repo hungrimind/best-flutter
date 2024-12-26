e04c2d087f28bc8c57a99992406b6ac2ea7d3ee4
Robert Brunhage <robertbrunhage@gmail.com>
2024-12-26 17:01:04 +0100

    after: row-column

--- row_column/lib/demo.dart
+++ row_column/lib/demo.dart
@@ -15,6 +15,13 @@ class LayoutDemo extends StatelessWidget {
             Text('by Hungrimind',
                 style: Theme.of(context).textTheme.titleMedium,
             ),
+            Row(
+              children: [
+                Container(width: 50, height: 50, color: Colors.red),
+                Container(width: 50, height: 50, color: Colors.green),
+                Container(width: 50, height: 50, color: Colors.blue),
+              ],
+            ),
           ],
         ),
       ),
