a9fdc5c4b713d36e653917504fc865b96f69fcc9
Tadas Petra <tadas@tadaspetra.com>
2025-02-01 09:33:49 -0600

    have the import ready

--- create_user/initial/lib/auth/create_account_view_model.dart
+++ create_user/initial/lib/auth/create_account_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:demo/auth/user.dart';
 import 'package:demo/auth/user_service.dart';
+import 'package:uuid/uuid.dart';
 
 class CreateAccountViewModel {
   CreateAccountViewModel(UserService userService) : _userService = userService;
