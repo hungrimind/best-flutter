f4cdf67a895ce7dedb7dc4d1202e61472e4fd51d
Robert Brunhage <robertbrunhage@gmail.com>
2025-02-02 14:55:10 +0100

    chore: remove unused import in initial folder

--- create_session/initial/test/home_test.dart
+++ create_session/initial/test/home_test.dart
@@ -1,7 +1,6 @@
 import 'package:demo/auth/user.dart';
 import 'package:demo/auth/user_service.dart';
 import 'package:demo/core/locator.dart';
-import 'package:demo/home/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
