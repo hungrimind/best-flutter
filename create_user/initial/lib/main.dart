5c67130d8c1286680c3fc3527120b88e615936a7
Tadas Petra <tadas@tadaspetra.com>
2025-02-03 07:26:47 -0600

    update all databases to have admin field

--- create_user/initial/lib/main.dart
+++ create_user/initial/lib/main.dart
@@ -10,7 +10,7 @@ Future<void> main() async {
 
   await locator<DatabaseAbstraction>().openDatabaseWithTables(
     [
-      'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, uid INTEGER NOT NULL)',
+      'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, uid INTEGER NOT NULL, admin INTEGER NOT NULL)',
       'CREATE TABLE IF NOT EXISTS sessions (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id))'
     ],
     'my_app',
